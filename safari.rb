380111b40f3693d69a8c7ad74e325e5b8b634699
Justin Muncaster <jmuncaster@toyon.com>
2014-11-28 22:38:24 -0800

    Refactor load and save so that I can understand it better.

--- safari.rb
+++ safari.rb
@@ -24,54 +24,95 @@ module Safari
     end
 
     def load(filename='~/Library/Safari/Bookmarks.plist')
+
       if filename.include? '~'
         filename.gsub!('~', ENV["HOME"])
       end
       plist = CFPropertyList::List.new(:file => filename)
       data = CFPropertyList.native_types(plist.value)
-      @other = data
-      index = 0
-      data["Children"].each_index do |i|
-        if data["Children"][i]["Title"] == "BookmarksBar"
-          index = i
+
+      self.debug_print_root data
+
+      puts "Parsing bookmarks"
+
+      @original = data
+      puts "  There are #{data['Children'].length} children"
+
+      all = data["Children"].dup
+
+      # Note special nodes: History and BookmarksBar
+      @history_node = all[0]
+      if not @history_node["Title"].eql? "History"
+        puts "  WARNING: First node title is #{bookmarks_node['Title']}, not BookmarksBar"
+      end
+      bookmarks_bar_node = all[1]
+      if not bookmarks_bar_node["Title"].eql? "BookmarksBar"
+        puts "  WARNING: First node title is #{bookmarks_node['Title']}, not BookmarksBar"
+      end
+
+      # Note other special nodes
+      @reserved_nodes = []
+      reserved_titles = []
+      reserved_list = ["Address Book", "Bonjour", "All RSS Feeds", "BookmarksMenu"]
+      all.each do |item|
+        title = item["Title"]
+        if reserved_list.include?(title)
+          puts "  Adding #{title} to reserved nodes list"
+          @reserved_nodes.push(item)
+          reserved_titles.push(title)
         end
       end
 
-      bookmarks_bar = SafariFolder.new_from_hash(data["Children"][index])
-      all = data["Children"].dup
-      toremove = []
-      all.each do |item|
-        if (!item["WebBookmarkIdentifier"].nil? || 
-          ["BookmarksMenu", "Address Book", "Bonjour", "All RSS Feeds", "BookmarksBar"].include?(item["Title"]))
-          toremove.push(item)
-        end
-      end
-      bookmarks = all - toremove
-      toremove.delete_at(index)
-      @extra = toremove
-      toadd = []
-      bookmarks.each do |bookmark|
-        if bookmark["WebBookmarkType"] == "WebBookmarkTypeLeaf" 
-          toadd.push(SafariBookmark.new_from_hash(bookmark))
+      # Note remaining nodes (all except reserved nodes)
+      other_nodes = all - [@history_node] - [bookmarks_bar_node] - @reserved_nodes
+
+      puts "  Found 1 history node: #{[@history_node['Title']].to_s}"
+      puts "  Found 1 bookmarks bar node: #{[bookmarks_bar_node['Title']].to_s}"
+      puts "  Found #{@reserved_nodes.length} reserved nodes: #{reserved_titles.to_s}"
+      puts "  Found #{other_nodes.length} other nodes"
+
+      # Create data structure for bookmarks bar
+      bookmarks_bar = SafariFolder.new_from_hash(bookmarks_bar_node)
+
+      # Create data structure for everything else
+      other_root = Folder.new("Other Bookmarks")
+      other_bookmarks = []
+      other_nodes.each do |node|
+        if node["WebBookmarkType"] == "WebBookmarkTypeLeaf"
+          other_bookmarks.push(SafariBookmark.new_from_hash(node))
         else
-          toadd.push(SafariFolder.new_from_hash(bookmark))
+          other_bookmarks.push(SafariFolder.new_from_hash(node))
         end
       end
-      other = Folder.new("Other Bookmarks")
-      other.add_from_array toadd
-      return {"bar" => bookmarks_bar, "other" => other}
+     other_root.add_from_array other_bookmarks
+
+      return {"bar" => bookmarks_bar, "other" => other_root}
     end
 
     def save(collection, filename='~/Library/Safari/Bookmarks.plist')
+
       if filename.include? '~'
         filename.gsub!('~', ENV["HOME"])
       end
-      outline = @other.dup
+
+      # Get all keys/values of root node.
+      outline = @original.dup
+
+      # Remove all children - we will rebuild.
       outline["Children"] = []
-      outline["Children"].push(folder_to_hash(collection.bookmarks_bar)) #First, as per Safari's preference
-      @extra.each do |item| 
-        outline["Children"].push item #Second, once again like Safari likes
+
+      # 0th node is actually History
+      outline["Children"].push @history_node
+
+      # First, as per Safari's preference - Add BookmarksBar
+      outline["Children"].push folder_to_hash(collection.bookmarks_bar)
+
+      # Second, once again like Safari likes - Add reserved nodes
+      @reserved_nodes.each do |item|
+        outline["Children"].push item
       end
+
+      # Last, add all other children
       collection.other.children.each do |item|
         if item.respond_to?(:url)
           outline["Children"].push(bookmark_to_hash(item))
@@ -79,9 +120,16 @@ module Safari
           outline["Children"].push(folder_to_hash(item))
         end
       end
+
+      # Debug
+      puts "  There are #{outline['Children'].length} children."
+
+      # Create plist
       plist = CFPropertyList::List.new
       plist.value = CFPropertyList.guess(outline) # data is native ruby structure
-      plist.save(filename, CFPropertyList::List::FORMAT_BINARY)
+
+      dry_run = true
+      plist.save(filename, CFPropertyList::List::FORMAT_BINARY) unless dry_run
     end
 
     private
