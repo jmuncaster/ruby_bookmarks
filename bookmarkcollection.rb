de01d2318f942becd3da57f3af1cc9e5f7d1b3d2
Justin Muncaster <jmuncaster@toyon.com>
2014-11-28 23:19:41 -0800

    Make output prettier

--- bookmarkcollection.rb
+++ bookmarkcollection.rb
@@ -32,35 +32,50 @@ class BookmarkCollection
   end
 
   def build_folder_hash
-    @folder_hash = {}
-    @bookmarks_bar.build_folder_hash @folder_hash
-    @other.build_folder_hash @folder_hash
+    folder_hash = {}
+    @bookmarks_bar.build_folder_hash folder_hash
+    @other.build_folder_hash folder_hash
 
-    puts "  Duplicate folders:"
-    @folder_hash.keys.each do |key|
-      puts "  #{key}: #{@folder_hash[key].length} dups" unless @folder_hash[key].length == 1
+    dups_folder_hash = {}
+    folder_hash.keys.each do |key|
+      if folder_hash[key].length > 1
+        dups_folder_hash[key] = folder_hash[key]
+      end
     end
+
+    puts "  Duplicate folders:" unless dups_folder_hash.empty?
+    dups_folder_hash.keys.each do |key|
+      puts "  #{key}: #{dups_folder_hash[key].length} dups"
+    end
+
+    @folder_hash = dups_folder_hash
+
   end
 
   def merge_folders
-    puts "  Merging folders"
-    @folder_hash.keys.each do |name|
-      puts "  Merging #{name}"
+    self.build_folder_hash
 
+    puts "  Merging folders" unless @folder_hash.empty?
+
+    @folder_hash.keys.each do |name|
       folders = @folder_hash[name]
 
-      merged = folders[0]
-      others = folders.slice(1, folders.length - 1)
+      if folders.length > 1
+        puts "  Merging #{name}"
+        merged = folders[0]
+        others = folders.slice(1, folders.length - 1)
 
-      others.each do |other|
-        puts "    Adding #{other.children.length} items."
-        merged.add_from_array other.children
-        other.clear
+        others.each do |other|
+          puts "    Adding #{other.children.length} items."
+          merged.add_from_array other.children
+          other.clear
+        end
+
+        puts "  Merged #{name}. Contains #{merged.children.length} items."
       end
-
-      puts "  Merged #{name}. Contains #{merged.children.length} items."
-
     end
+
+    self.build_folder_hash
   end
 
   def to_s
