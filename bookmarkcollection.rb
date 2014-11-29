d3990f722d076203241e35485891d41fccfa0692
Justin Muncaster <jmuncaster@toyon.com>
2014-11-28 22:39:49 -0800

    Add to_s

--- bookmarkcollection.rb
+++ bookmarkcollection.rb
@@ -1,6 +1,6 @@
 class BookmarkCollection
   attr_accessor :bookmarks_bar, :other, :providers
-  
+
   def initialize(providers)
     @bookmarks_bar = Folder.new("bookmarks_bar")
     @other = Folder.new("Other Bookmarks")
@@ -23,11 +23,15 @@ class BookmarkCollection
 
   def all(&block)
     @bookmarks_bar.all_children &block
-    @other.all_children &block  
+    @other.all_children &block
+  end
+
   def remove_empty_folders
     @bookmarks_bar.remove_empty_folders
     @other.remove_empty_folders
   end
 
+  def to_s
+    @bookmarks_bar.to_s + "\n" + @other.to_s
   end
 end
